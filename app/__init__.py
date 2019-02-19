7b867bfcc59f477494a008669dcadb13f9a5ffb7
BruceFrankWang <bruce.frank.wang@gmail.com>
2019-02-20 01:56:28 +0800

    Add some Flask extensions.

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ import os
 from flask import Flask
 
 from app.config import config
+from app.extensions import db, moment, migrate
 
 
 def create_app(config_name=None):
@@ -29,12 +30,18 @@ def create_app(config_name=None):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    register_logging(app)
+    # register_logging(app)
     register_extensions(app)
-    register_blueprints(app)
+    # register_blueprints(app)
     # register_errors(app)
-    register_commands(app)
-    register_shell_context(app)
+    # register_commands(app)
+    # register_shell_context(app)
     # register_template_context(app)
     # register_request_handlers(app)
     return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app)
+    moment.init_app(app)
---
+++ app/extensions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+    DODOA
+        A better OA system for Design Management.
+    
+    :Author:    Bruce Frank Wang <bruce.frank.wang@gmail.com>
+    :Time:      2019-02-20 01:20
+    :copyright: © 2019 Bruce Frank Wang <bruce.frank.wang@gmail.com>
+    :license:   MIT, see LICENSE for more details.
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_moment import Moment
+
+db = SQLAlchemy()
+migrate = Migrate()
+moment = Moment()
